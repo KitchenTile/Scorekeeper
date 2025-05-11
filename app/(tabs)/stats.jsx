0099f26fb6ef1e87bc212a342daaabb20c260bab
Blue <azuldebenedetti@gmail.com>
2025-05-11 09:27:23 +0100

    Update stats.jsx

--- app/(tabs)/stats.jsx
+++ app/(tabs)/stats.jsx
@@ -1,8 +1,175 @@
-import React from "react";
-import { View } from "react-native-web";
+import React, { useEffect, useState } from "react";
+import { ScrollView, View, Text } from "react-native-web";
+import { useMatchStore } from "../../store";
+import { StyleSheet } from "react-native";
+import { TouchableOpacity } from "react-native";
+import { AntDesign } from "@expo/vector-icons";
 
 const stats = () => {
-  return <View>alksfuf</View>;
+  const [activeTab, setActiveTab] = useState(1);
+  const sets = useMatchStore((state) => state.sets);
+
+  useEffect(() => {
+    console.log(activeTab);
+  }, [activeTab]);
+
+  return (
+    <ScrollView contentContainerStyle={styles.container}>
+      {sets.map((set, index) => (
+        <View
+          style={[
+            styles.pointInfoContainer,
+            {
+              height: index === activeTab ? 300 : "",
+              display: "flex",
+              flexDirection: "column",
+            },
+          ]}
+        >
+          <View
+            style={{
+              maxHeight: 80,
+              display: "flex",
+              flexDirection: "row",
+              justifyContent: "space-around",
+            }}
+          >
+            <Text style={styles.title}>SET {index + 1} BREAKDOWN</Text>
+            <TouchableOpacity
+              style={[styles.optionButton, { textAlign: "center" }]}
+              onPress={() => setActiveTab(index)}
+            >
+              <AntDesign name="caretdown" size={24} color="white" />{" "}
+            </TouchableOpacity>
+          </View>
+        </View>
+      ))}
+    </ScrollView>
+  );
 };
 
 export default stats;
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: "#161F23",
+    flex: 1,
+    flexDirection: "column",
+    marginTop: 50,
+    padding: 20,
+    // maxWidth: "800px",
+  },
+
+  title: {
+    fontSize: 22,
+    fontWeight: "bold",
+    marginBottom: 20,
+    textAlign: "center",
+    color: "white",
+  },
+
+  pointInfoContainer: {
+    width: "100%",
+    height: 80,
+    borderWidth: 2,
+    borderColor: "#3A464E",
+    borderRadius: 20,
+    marginTop: 20,
+    padding: 10,
+    marginInline: "auto",
+  },
+
+  infoContainer: {
+    width: "90%",
+    height: 170,
+    position: "absolute",
+    bottom: "12.5%",
+    left: "5%",
+    borderWidth: 2,
+    borderColor: "#3A464E",
+    borderRadius: 20,
+    // overflow: "hidden",
+    marginInline: "auto",
+    flexDirection: "row",
+    justifyContent: "space-between",
+  },
+
+  bttnsContainer: {
+    position: "absolute",
+    bottom: "3%",
+    width: "90%",
+    left: "5%",
+    flexDirection: "row",
+    justifyContent: "space-between",
+    height: 40,
+    marginBlock: 20,
+  },
+
+  bttn: {
+    width: "48.5%",
+    height: 65,
+    backgroundColor: "#78C93C",
+    borderWidth: 2,
+    borderColor: "#488719",
+    position: "relative",
+    paddingBlock: 10,
+    paddingInline: 20,
+    borderRadius: 15,
+  },
+
+  bttnOpp: {
+    width: "48.5%",
+    height: 65,
+    backgroundColor: "#DC605B",
+    borderWidth: 2,
+    borderColor: "#B52924",
+    position: "relative",
+    paddingBlock: 10,
+    paddingInline: 20,
+    borderRadius: 15,
+  },
+
+  bttnTxt: {
+    position: "absolute",
+    left: "50%",
+    top: "50%",
+    transform: "translate(-50%, -60%)",
+    fontSize: "34px",
+    lineHeight: "28px",
+    textAlign: "center",
+    color: "rgb(0,0,0)",
+    fontWeight: 600,
+  },
+
+  optionButton: {
+    backgroundColor: "#586DFF",
+    padding: 10,
+    margin: 5,
+    borderRadius: 5,
+    minWidth: 50,
+    minHeight: 50,
+    borderStyle: "solid",
+    borderWidth: 2,
+    borderColor: "#3c4cbb",
+  },
+
+  text: {
+    fontSize: 42,
+    fontWeight: 600,
+    textAlign: "center",
+  },
+
+  infoIcon: {
+    position: "absolute",
+    top: "-5%",
+    left: "95%",
+    borderRadius: "100%",
+    width: 30,
+    height: 30,
+    backgroundColor: "#161F23",
+    borderStyle: "solid",
+    borderWidth: 2,
+    borderColor: "#3A464E",
+    zIndex: 10,
+  },
+});
