beb81e4b1e0483a2f5efb21cd234b36bf2173054
Blue <azuldebenedetti@gmail.com>
2025-05-11 18:47:58 +0100

    Create PlayersGraphs.jsx

---
+++ components/stats_components/PlayersGraphs.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { ScrollView, View, Text } from "react-native-web";
+import { useMatchStore } from "../../store";
+import { StyleSheet } from "react-native";
+
+const PlayersGraphs = () => {
+  return <View></View>;
+};
+
+export default PlayersGraphs;
