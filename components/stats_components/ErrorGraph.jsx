8db6825a63b7a01bb7057bb27a6a361685fce19c
Blue <azuldebenedetti@gmail.com>
2025-05-14 23:57:26 +0100

    Create ErrorGraph.jsx

---
+++ components/stats_components/ErrorGraph.jsx
@@ -0,0 +1,83 @@
+import React from "react";
+import { ScrollView, View, Text } from "react-native-web";
+import { useMatchStore } from "../../store";
+import { StyleSheet } from "react-native";
+import { PieChart } from "react-native-chart-kit";
+import { Dimensions } from "react-native";
+
+const ErrorGraph = ({ set }) => {
+  const errorOrganizer = (sets) => {
+    const errorObj = {};
+    MISTAKEMETHODS.forEach((e) => {
+      errorObj[e] = 0;
+    });
+
+    for (let i = 0; i < sets.lineChartScore.length; i++) {
+      if (sets.lineChartScore[i].isMistake) {
+        if (MISTAKEMETHODS.includes(sets.lineChartScore[i].method)) {
+          errorObj[sets.lineChartScore[i].method]++;
+        }
+      }
+    }
+
+    return errorObj;
+  };
+
+  console.log(errorOrganizer(set));
+
+  const data = Object.entries(errorOrganizer(set)).map(
+    ([key, value], index) => {
+      const hue = 1 + index * 4; // small step away from base red
+      const lightness = Math.max(10, 80 - value * 5); // darker with more errors
+
+      return {
+        name: key,
+        errorQty: value,
+        color: `hsl(${hue}, 100%, ${lightness}%)`,
+        legendFontColor: "#7F7F7F",
+        legendFontSize: 15,
+      };
+    }
+  );
+
+  console.log(data);
+
+  const chartConfig = {
+    backgroundGradientFrom: "#1E2923",
+    backgroundGradientFromOpacity: 0,
+    backgroundGradientTo: "#08130D",
+    backgroundGradientToOpacity: 0.5,
+    color: (opacity = 1) => `rgba(26, 255, 146, ${opacity})`,
+    strokeWidth: 3, // optional, default 3
+    barPercentage: 0.8,
+  };
+
+  return (
+    <View>
+      <PieChart
+        data={data}
+        width={Dimensions.get("window").width * 0.85}
+        height={220}
+        chartConfig={chartConfig}
+        accessor={"errorQty"}
+        backgroundColor={"transparent"}
+        // paddingLeft={"15"}
+        // center={[10, 50]}
+        absolute
+      />
+    </View>
+  );
+};
+
+export default ErrorGraph;
+
+const MISTAKEMETHODS = [
+  "Communication",
+  "Passing Error",
+  "Serving Error",
+  "Hitting Error",
+  //   "undefined",
+  //   "null",
+  "Free ball",
+  "Court Coverage",
+];
