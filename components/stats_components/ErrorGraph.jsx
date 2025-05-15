143cbd16b2514f5a02ffd4a2a58ac0743cde8a79
Blue <azuldebenedetti@gmail.com>
2025-05-15 13:49:38 +0100

    Update ErrorGraph.jsx

--- components/stats_components/ErrorGraph.jsx
+++ components/stats_components/ErrorGraph.jsx
@@ -2,14 +2,21 @@ import React from "react";
 import { ScrollView, View, Text } from "react-native-web";
 import { useMatchStore } from "../../store";
 import { StyleSheet } from "react-native";
-import { PieChart } from "react-native-chart-kit";
+import { PieChart, BarChart } from "react-native-chart-kit";
 import { Dimensions } from "react-native";
 
 const ErrorGraph = ({ set }) => {
-  const errorOrganizer = (sets) => {
+  const players = useMatchStore((state) => state.players);
+
+  const errorOrganizer = (players, sets) => {
     const errorObj = {};
     MISTAKEMETHODS.forEach((e) => {
       errorObj[e] = 0;
+    });
+
+    const playersObj = {};
+    players.forEach((e) => {
+      playersObj[e] = 0;
     });
 
     for (let i = 0; i < sets.lineChartScore.length; i++) {
@@ -17,18 +24,21 @@ const ErrorGraph = ({ set }) => {
         if (MISTAKEMETHODS.includes(sets.lineChartScore[i].method)) {
           errorObj[sets.lineChartScore[i].method]++;
         }
+        if (players.includes(sets.lineChartScore[i].author)) {
+          playersObj[sets.lineChartScore[i].author]++;
+        }
       }
     }
 
-    return errorObj;
+    return { errorObj, playersObj };
   };
 
-  console.log(errorOrganizer(set));
+  console.log(errorOrganizer(players, set));
 
-  const data = Object.entries(errorOrganizer(set)).map(
+  const pieData = Object.entries(errorOrganizer(players, set).errorObj).map(
     ([key, value], index) => {
-      const hue = 1 + index * 4; // small step away from base red
-      const lightness = Math.max(10, 80 - value * 5); // darker with more errors
+      const hue = 1 + index * 4;
+      const lightness = Math.max(10, 80 - value * 5);
 
       return {
         name: key,
@@ -40,7 +50,24 @@ const ErrorGraph = ({ set }) => {
     }
   );
 
-  console.log(data);
+  const barData = {
+    labels: Object.entries(errorOrganizer(players, set).playersObj).map(
+      ([key, value]) => {
+        return key;
+      }
+    ),
+    datasets: [
+      {
+        data: Object.entries(errorOrganizer(players, set).playersObj).map(
+          ([key, value]) => {
+            return value;
+          }
+        ),
+      },
+    ],
+  };
+
+  console.log(barData);
 
   const chartConfig = {
     backgroundGradientFrom: "#1E2923",
@@ -48,24 +75,36 @@ const ErrorGraph = ({ set }) => {
     backgroundGradientTo: "#08130D",
     backgroundGradientToOpacity: 0.5,
     color: (opacity = 1) => `rgba(26, 255, 146, ${opacity})`,
-    strokeWidth: 3, // optional, default 3
+    strokeWidth: 3,
     barPercentage: 0.8,
   };
 
   return (
-    <View>
-      <PieChart
-        data={data}
-        width={Dimensions.get("window").width * 0.85}
-        height={220}
-        chartConfig={chartConfig}
-        accessor={"errorQty"}
-        backgroundColor={"transparent"}
-        // paddingLeft={"15"}
-        // center={[10, 50]}
-        absolute
-      />
-    </View>
+    <>
+      <Text>Error Graphs</Text>
+      <View>
+        <PieChart
+          data={pieData}
+          width={Dimensions.get("window").width * 0.85}
+          height={220}
+          chartConfig={chartConfig}
+          accessor={"errorQty"}
+          backgroundColor={"transparent"}
+          // paddingLeft={"15"}
+          // center={[10, 50]}
+          absolute
+        />
+      </View>
+      <View>
+        <BarChart
+          // style={graphStyle}
+          data={barData}
+          width={Dimensions.get("window").width * 0.85}
+          height={220}
+          chartConfig={chartConfig}
+        />
+      </View>
+    </>
   );
 };
 
