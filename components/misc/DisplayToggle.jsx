36d30a03ae2c503737d6ea3b9df5088958179269
Blue <azuldebenedetti@gmail.com>
2025-06-01 12:42:36 +0100

    Update DisplayToggle.jsx

---
+++ components/misc/DisplayToggle.jsx
@@ -0,0 +1,59 @@
+import { useEffect } from "react";
+
+import React from "react";
+import { ScrollView, View, Text } from "react-native-web";
+import { useMatchStore } from "../../store";
+import { StyleSheet } from "react-native";
+import { BarChart, PieChart } from "react-native-chart-kit";
+import { Dimensions } from "react-native";
+import { playerPointsAcrossSetsOrganizer } from "../../utils/statsProcessor";
+import { TouchableOpacity } from "react-native";
+
+const DisplayToggle = ({ label, active, onPress }) => {
+  return (
+    <TouchableOpacity
+      style={[
+        styles.optionButton,
+        {
+          backgroundColor: active === "team" ? "#586DFF" : "transparent",
+        },
+      ]}
+      onPress={onPress}
+    >
+      <Text
+        style={[
+          styles.bttnTxt,
+          {
+            color: active === "team" ? "white" : "#586DFF",
+          },
+        ]}
+      >
+        {label}
+      </Text>
+    </TouchableOpacity>
+  );
+};
+
+export default DisplayToggle;
+
+const styles = StyleSheet.create({
+  optionButton: {
+    backgroundColor: "#586DFF",
+    padding: 3,
+    paddingTop: 0,
+    marginBottom: 15,
+    borderRadius: 5,
+    minWidth: "48%",
+    minHeight: 40,
+    borderStyle: "solid",
+    borderWidth: 2,
+    borderColor: "#3c4cbb",
+  },
+
+  bttnTxt: {
+    fontSize: 22,
+    textAlign: "center",
+    color: "white",
+    lineHeight: 35,
+  },
+});
