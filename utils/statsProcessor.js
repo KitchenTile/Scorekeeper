a623cd6ce86975ffb87cfb00a5375e1c68a1e1dc
Blue <azuldebenedetti@gmail.com>
2025-06-01 18:20:20 +0100

    Update statsProcessor.js

--- utils/statsProcessor.js
+++ utils/statsProcessor.js
@@ -103,3 +103,38 @@ export const playerPointsAcrossSetsOrganizer = (sets, player) => {
 
   return { pointObj, pointObjPerSet, playerPointsPerSet };
 };
+
+export const playerErrorAcrossSetsOrganizer = (sets, player) => {
+  const errorObj = {};
+  let arr = Array.from({ length: sets.length }, (element, index) => index + 1);
+  const playerErrorsPerSet = {};
+  const errorObjPerSet = sets.map(() => {
+    const obj = {};
+    MISTAKEMETHODS.forEach((method) => (obj[method] = 0));
+    return obj;
+  });
+
+  arr.map((set) => {
+    playerErrorsPerSet[set] = 0;
+  });
+
+  MISTAKEMETHODS.forEach((point) => {
+    errorObj[point] = 0;
+  });
+
+  sets.forEach((set, index) => {
+    for (let i = 0; i < set.lineChartScore.length; i++) {
+      if (
+        set.lineChartScore[i].isMistake &&
+        MISTAKEMETHODS.includes(set.lineChartScore[i].method) &&
+        set.lineChartScore[i].author === player
+      ) {
+        errorObj[set.lineChartScore[i].method]++;
+        errorObjPerSet[index][set.lineChartScore[i].method]++;
+        playerErrorsPerSet[index + 1]++;
+      }
+    }
+  });
+
+  return { errorObj, errorObjPerSet, playerErrorsPerSet };
+};
