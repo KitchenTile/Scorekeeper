9565e79d0c65f4c816199a2b1749e22fa5a0e748
Blue <azuldebenedetti@gmail.com>
2025-07-10 14:00:52 +0100

    Create MatchHistryCards.jsx

---
+++ components/cards/MatchHistryCards.jsx
@@ -0,0 +1,75 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+
+const MatchHistryCards = ({ match }) => {
+  const matchDate = new Date(match.time_created);
+
+  //   const gameSets = Object.fromEntries(
+  //     Object.entries(match.gameSets).map(([key, value]) => {
+  //       return [key, value];
+  //     })
+  //   );
+
+  const getSetResults = () => {
+    const setResults = {};
+    match.teams.forEach((team) => {
+      setResults[team] = 0;
+    });
+
+    Object.fromEntries(
+      Object.entries(gameSets).map(([key, value]) => {
+        setResults[value.winner]++;
+        return [key, value];
+      })
+    );
+
+    return setResults;
+  };
+
+  console.log(gameSets);
+  console.log(getSetResults());
+
+  return (
+    <View style={styles.cardContainer}>
+      <Text
+        style={{
+          color: match.match_winner === match.teams[0] ? "white" : "#3A464E",
+        }}
+      >
+        {match.teams[0]}
+      </Text>
+      <Text
+        style={{
+          color: match.match_winner === match.teams[1] ? "white" : "#3A464E",
+        }}
+      >
+        {match.teams[1]}
+      </Text>
+      <Text style={{ color: "white" }}>
+        {matchDate.toLocaleString("en-GB", {
+          timeZone: "UTC",
+          day: "2-digit",
+          month: "2-digit",
+          year: "numeric",
+          hour: "2-digit",
+          minute: "2-digit",
+          hour12: false,
+        })}
+      </Text>
+    </View>
+  );
+};
+
+export default MatchHistryCards;
+
+const styles = StyleSheet.create({
+  cardContainer: {
+    width: "100%",
+    height: 100,
+    borderWidth: 2,
+    borderColor: "#3A464E",
+    borderRadius: 20,
+    marginTop: 15,
+    padding: 10,
+  },
+});
