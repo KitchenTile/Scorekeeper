d713303e77034451391ff5073bd14ecbd41b392b
Blue <azuldebenedetti@gmail.com>
2025-07-14 15:45:56 +0100

    Update PrevMatchModal.jsx

--- components/stats_components/PrevMatchModal.jsx
+++ components/stats_components/PrevMatchModal.jsx
@@ -5,11 +5,44 @@ import { Modal } from "react-native-web";
 const PrevMatchModal = ({ isVisible, match }) => {
   return (
     <Modal visible={isVisible} transparent animationType="slide">
-      <Text>{match.teams[0]}</Text>
+      <View style={styles.modalBackground}>
+        <View style={styles.modalContent}>
+          <Text style={styles.modalTxt}>{match.teams[0]}</Text>
+        </View>
+      </View>
     </Modal>
   );
 };
 
 export default PrevMatchModal;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  modalBackground: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "rgba(0,0,0,0.5)",
+  },
+
+  modalContent: {
+    width: "90%",
+    minHeight: 500,
+    gap: 20,
+    backgroundColor: "#161F23",
+    paddingBlock: 20,
+    borderRadius: 10,
+    alignItems: "center",
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "space-between",
+    borderWidth: 2,
+    borderColor: "#3A464E",
+  },
+
+  modalTxt: {
+    fontSize: 18,
+    color: "white",
+    fontWeight: "bold",
+    marginBottom: 10,
+  },
+});
