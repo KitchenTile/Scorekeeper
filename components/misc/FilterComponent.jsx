daca87e5e02ca329eb5c6759e0b5c4e419847cc3
Blue <azuldebenedetti@gmail.com>
2025-07-22 17:53:57 +0100

    Update FilterComponent.jsx

--- components/misc/FilterComponent.jsx
+++ components/misc/FilterComponent.jsx
@@ -1,6 +1,7 @@
 import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
 import React from "react";
 import ButtonRow from "./ButtonRow";
+import PlayerButtonRow from "./PlayerButtonRow";
 
 const FilterComponent = ({
   onStatChange,
@@ -12,50 +13,28 @@ const FilterComponent = ({
   players,
 }) => {
   return (
-    <View
-      style={{
-        flexDirection: "row",
-        justifyContent: "start",
-        gap: 10,
-        marginBottom: 15,
-      }}
-    >
-      <ButtonRow
-        active={statView}
-        onChange={(val) => onStatChange(val)}
-        labels={["team", "players"]}
-        icons={["users", "user"]}
-      />
-      <ButtonRow
-        active={pointOrError}
-        onChange={(val) => onPointToggle(val)}
-        labels={["points", "errors"]}
-        icons={["check", "close"]}
-      />
-      {statView === "players" && (
+    <View style={{ flexDirection: "column" }}>
+      <View style={styles.wrapper}>
         <ButtonRow
-          active={selectedPlayer}
-          onChange={(val) => onSelectPlayer(val)}
+          active={statView}
+          onChange={(val) => onStatChange(val)}
+          labels={["team", "players"]}
+          icons={["users", "user"]}
+        />
+        <ButtonRow
+          active={pointOrError}
+          onChange={(val) => onPointToggle(val)}
+          labels={["points", "errors"]}
+          icons={["check", "close"]}
+        />
+      </View>
+      {statView === "players" && (
+        <PlayerButtonRow
           labels={players}
-          direction={"column"}
-          width={47}
+          onChange={(val) => onSelectPlayer(val)}
+          active={selectedPlayer}
         />
       )}
-      {/* 
-      <View style={styles.toggleRow}>
-        {players.map((player) => (
-          <TouchableOpacity
-            key={player}
-            style={[
-              styles.playerBtn,
-              selectedPlayer === player && styles.selected,
-            ]}
-            onPress={() => onSelectPlayer(player)}
-          >
-            <Text style={styles.playerText}>{player}</Text>
-          </TouchableOpacity>
-        ))}
-      </View> */}
     </View>
   );
 };
@@ -63,26 +42,10 @@ const FilterComponent = ({
 export default FilterComponent;
 
 const styles = StyleSheet.create({
-  playerBtn: {
-    backgroundColor: "#586DFF",
-    // padding: 10,
-    margin: 5,
-    marginBlock: 10,
-    borderRadius: 5,
-    minWidth: 47,
-    minHeight: 47,
-    borderStyle: "solid",
-    borderWidth: 2,
-    borderColor: "#3c4cbb",
-  },
-  selected: {
-    backgroundColor: "#28a745",
-    borderColor: "#28a745",
-  },
-  playerText: {
-    color: "white",
-    fontSize: 20,
-    textAlign: "center",
-    lineHeight: 47,
+  wrapper: {
+    flexDirection: "row",
+    justifyContent: "start",
+    gap: 10,
+    marginBottom: 15,
   },
 });
