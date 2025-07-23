6826556534969f3b2785cfa2ebbcf6f316309344
Blue <azuldebenedetti@gmail.com>
2025-07-23 16:07:03 +0100

    Update PlayerButtonRow.jsx

--- components/misc/PlayerButtonRow.jsx
+++ components/misc/PlayerButtonRow.jsx
@@ -7,11 +7,15 @@ import {
   View,
 } from "react-native";
 import React, { useRef, useState } from "react";
+import { useMatchStore } from "../../stores/store";
 
 const PlayerButtonRow = ({ labels, active, onChange }) => {
   const [open, setOpen] = useState(true);
 
-  console.log(active);
+  const openedButton = useMatchStore((state) => state.openedButton);
+
+  const sideIds = ["points", "errors", "team", "players"];
+  const isSideOpen = sideIds.includes(openedButton.active);
 
   const slideAnim = useRef(new Animated.Value(0)).current;
 
@@ -32,36 +36,38 @@ const PlayerButtonRow = ({ labels, active, onChange }) => {
   });
 
   return (
-    <View>
-      <TouchableOpacity
-        onPress={toggleOpen}
-        style={[
-          styles.playerBtn,
-          { position: "absolute", top: -65, left: 235 },
-        ]}
-      >
-        <Text style={styles.playerText}>{active}</Text>
-      </TouchableOpacity>
-      <Animated.View
-        style={[
-          styles.toggleRow,
-          { height: heightInterpolate, opacity: slideAnim },
-        ]}
-      >
-        {labels.map((player) => (
-          <TouchableOpacity
-            key={player}
-            style={[styles.playerBtn, active === player && styles.selected]}
-            onPress={() => {
-              onChange(player);
-              toggleOpen();
-            }}
-          >
-            <Text style={styles.playerText}>{player}</Text>
-          </TouchableOpacity>
-        ))}
-      </Animated.View>
-    </View>
+    !isSideOpen && (
+      <View>
+        <TouchableOpacity
+          onPress={toggleOpen}
+          style={[
+            styles.playerBtn,
+            { position: "absolute", top: -58, left: 236 },
+          ]}
+        >
+          <Text style={styles.playerText}>{active}</Text>
+        </TouchableOpacity>
+        <Animated.View
+          style={[
+            styles.toggleRow,
+            { height: heightInterpolate, opacity: slideAnim },
+          ]}
+        >
+          {labels.map((player) => (
+            <TouchableOpacity
+              key={player}
+              style={[styles.playerBtn, active === player && styles.selected]}
+              onPress={() => {
+                onChange(player);
+                toggleOpen();
+              }}
+            >
+              <Text style={styles.playerText}>{player}</Text>
+            </TouchableOpacity>
+          ))}
+        </Animated.View>
+      </View>
+    )
   );
 };
 
@@ -71,6 +77,7 @@ const styles = StyleSheet.create({
   wrapper: {},
   toggleRow: {
     flexDirection: "row",
+    justifyContent: "space-between",
   },
   playerBtn: {
     margin: 5,
