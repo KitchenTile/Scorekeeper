4f932d3b24847fdc37205498101af3ea4e8364ff
Blue <azuldebenedetti@gmail.com>
2025-08-08 13:52:49 +0100

    Update babel.config.js

--- babel.config.js
+++ babel.config.js
@@ -5,6 +5,19 @@
 //   };
 // };
 
+// module.exports = {
+//   presets: ["module:metro-react-native-babel-preset"],
+//   plugins: [
+//     [
+//       "module:react-native-dotenv",
+//       {
+//         moduleName: "@env",
+//         path: ".env",
+//       },
+//     ],
+//   ],
+// };
+
 module.exports = {
   presets: ["module:metro-react-native-babel-preset"],
   plugins: [
@@ -13,6 +26,10 @@ module.exports = {
       {
         moduleName: "@env",
         path: ".env",
+        blacklist: null,
+        whitelist: null,
+        safe: false,
+        allowUndefined: true,
       },
     ],
   ],
