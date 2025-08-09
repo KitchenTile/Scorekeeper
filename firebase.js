e4d8cac5bc9dc26e700ba80713910c08a74e236d
Blue <azuldebenedetti@gmail.com>
2025-08-09 10:15:50 +0100

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -2,16 +2,6 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
-// import {
-//   EXPO_PUBLIC_FIREBASE_API_KEY,
-//   EXPO_PUBLIC_FIREBASE_APP_ID,
-//   EXPO_PUBLIC_FIREBASE_AUTH_DOMAIN,
-//   EXPO_PUBLIC_FIREBASE_MEASUREMENT_ID,
-//   EXPO_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-//   EXPO_PUBLIC_FIREBASE_PROJECT_ID,
-//   EXPO_PUBLIC_FIREBASE_STOREBUCKET,
-// } from ".env";
-
 const firebaseConfig = {
   apiKey: process.env.EXPO_PUBLIC_FIREBASE_API_KEY,
   authDomain: process.env.EXPO_PUBLIC_FIREBASE_AUTH_DOMAIN,
@@ -21,16 +11,6 @@ const firebaseConfig = {
   appId: process.env.EXPO_PUBLIC_FIREBASE_APP_ID,
   measurementId: process.env.EXPO_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
-
-// const firebaseConfig = {
-//   apiKey: "AIzaSyDTgE9HztFGlOyado6clRouR6pT1T1dIao",
-//   authDomain: "vb-scorekeeper-659a7.firebaseapp.com",
-//   projectId: "vb-scorekeeper-659a7",
-//   storageBucket: "vb-scorekeeper-659a7.firebasestorage.app",
-//   messagingSenderId: "1022064983531",
-//   appId: "1:1022064983531:web:3db6c18c50ebaf65616dae",
-//   measurementId: "G-QGSBHHQ2GZ",
-// };
 
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
