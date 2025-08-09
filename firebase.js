fef98576a155c5efebecbcb8191ffd0824f9c578
Blue <azuldebenedetti@gmail.com>
2025-08-09 10:15:11 +0100

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,25 +1,36 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
-import {
-  FIREBASE_API_KEY,
-  FIREBASE_APP_ID,
-  FIREBASE_AUTH_DOMAIN,
-  FIREBASE_MEASUREMENT_ID,
-  FIREBASE_MESSAGING_SENDER_ID,
-  FIREBASE_PROJECT_ID,
-  FIREBASE_STOREBUCKET,
-} from "@env";
+
+// import {
+//   EXPO_PUBLIC_FIREBASE_API_KEY,
+//   EXPO_PUBLIC_FIREBASE_APP_ID,
+//   EXPO_PUBLIC_FIREBASE_AUTH_DOMAIN,
+//   EXPO_PUBLIC_FIREBASE_MEASUREMENT_ID,
+//   EXPO_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+//   EXPO_PUBLIC_FIREBASE_PROJECT_ID,
+//   EXPO_PUBLIC_FIREBASE_STOREBUCKET,
+// } from ".env";
 
 const firebaseConfig = {
-  apiKey: FIREBASE_API_KEY,
-  authDomain: FIREBASE_AUTH_DOMAIN,
-  projectId: FIREBASE_PROJECT_ID,
-  storageBucket: FIREBASE_STOREBUCKET,
-  messagingSenderId: FIREBASE_MESSAGING_SENDER_ID,
-  appId: FIREBASE_APP_ID,
-  measurementId: FIREBASE_MEASUREMENT_ID,
+  apiKey: process.env.EXPO_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.EXPO_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.EXPO_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.EXPO_PUBLIC_FIREBASE_STOREBUCKET,
+  messagingSenderId: process.env.EXPO_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.EXPO_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.EXPO_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
+
+// const firebaseConfig = {
+//   apiKey: "AIzaSyDTgE9HztFGlOyado6clRouR6pT1T1dIao",
+//   authDomain: "vb-scorekeeper-659a7.firebaseapp.com",
+//   projectId: "vb-scorekeeper-659a7",
+//   storageBucket: "vb-scorekeeper-659a7.firebasestorage.app",
+//   messagingSenderId: "1022064983531",
+//   appId: "1:1022064983531:web:3db6c18c50ebaf65616dae",
+//   measurementId: "G-QGSBHHQ2GZ",
+// };
 
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
